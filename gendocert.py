1d60ac23c5aca8742f545ce3787ec78f1ee1ee4e
Dave Mangot <github@mangot.com>
2014-11-10 13:38:21 -0800

    noted should be more diligent about the fs

--- gendocert.py
+++ gendocert.py
@@ -10,12 +10,14 @@ def draw_pdf(sparklydevop):
     certimage = './devops.cert.png'
 
     # TODO make this a function of image size
+    # TODO make sure we can read the file
     c = canvas.Canvas(OUTPUTFILE, pagesize=(1147, 1608))
     c.drawImage(certimage, 1, 1)
     c.setFont("Helvetica", 72)
     # TODO: calculate X for string based on length of username
     c.drawString(375, 1175, sparklydevop)
     c.showPage()
+    # TODO check for write permissions/failure
     c.save()
 
 
